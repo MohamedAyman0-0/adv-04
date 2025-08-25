eb7bbeb8911475f77403398e186577ca63aff62b
mohamed ayman <mohamedaymanabdel3ty@gmail.com>
2025-08-25 20:39:52 +0300

    Q1 Count Frequency of Each Element

--- adv 04/Program.cs
+++ adv 04/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using System.Collections;
+
+namespace Adv_4
+{
+    internal class Program
+    {
+        #region Q1 Count Frequency of Each Element
+        static void CountFrequencies(int[] arr)
+        {
+            var freq = new Dictionary<int, int>();
+            foreach (int num in arr)
+                if (freq.ContainsKey(num)) freq[num]++;
+                else freq[num] = 1;
+
+            foreach (var kv in freq)
+                Console.WriteLine($"{kv.Key} : {kv.Value}");
+
+        }
+        #endregion
+
+     
+        }
+    }
+
